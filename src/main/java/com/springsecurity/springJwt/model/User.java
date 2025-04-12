add52746e54ca4fdf24a6420c2c3b09f03349323
rahulyadav753 <ry1951947@gmail.com>
2025-04-13 03:50:10 +0530

    Database connecton establish and user entity class

---
+++ src/main/java/com/springsecurity/springJwt/model/User.java
@@ -0,0 +1,23 @@
+package com.springsecurity.springJwt.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "username")
+    String username;
+    @Column(name = "password")
+    String password;
+}
