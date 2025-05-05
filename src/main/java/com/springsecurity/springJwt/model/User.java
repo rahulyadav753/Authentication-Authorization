dd018ddcd0e7f15e86c640a6c0645243427c1df9
rahulyadav753 <ry1951947@gmail.com>
2025-05-06 02:33:41 +0530

    roles added

--- src/main/java/com/springsecurity/springJwt/model/User.java
+++ src/main/java/com/springsecurity/springJwt/model/User.java
@@ -20,4 +20,58 @@ public class User {
     String username;
     @Column(name = "password")
     String password;
+
+
+@Enumerated(value = EnumType.STRING)
+
+
+    Role role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
